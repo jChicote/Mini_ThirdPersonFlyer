a03ca5d8d735a75ba0377fab204447b431f13dde
Jaiden Chicote <jaiden_chicote@outlook.com>
2023-05-26 08:17:46 +1000

    Add rotation behaviour for the flight manager - #2

--- Assets/ThirdPersonFlyer/CoreSystems/FlightManager.cs
+++ Assets/ThirdPersonFlyer/CoreSystems/FlightManager.cs
@@ -12,6 +12,9 @@ namespace ThirdPersonFlyer.CoreSystems
 
         // References
         private Rigidbody m_FlightRigidbody;
+        private Transform m_CameraTransform;
+        private Transform m_CameraRigTransform;
+        private Transform m_CameraViewAimTransform;
 
         // Default values
         private float m_DefaultThrottlePower;
@@ -21,6 +24,7 @@ namespace ThirdPersonFlyer.CoreSystems
         private float m_Speed;
         private Vector3 m_Velocity;
         private float m_Sensitivity;
+        private float m_CameraSmoothSpeed;
 
         private Vector3 m_MouseAimPosition;
 
@@ -55,20 +59,40 @@ namespace ThirdPersonFlyer.CoreSystems
         void Update()
         {
             this.RotateFlyer();
+            this.MoveFlyer();
         }
 
-        public void RotateFlyer()
+        public void RotateFlyer(Vector2 screenPosition)
         {
             // Scale the input result
+            Vector2 _ScaledPosition = screenPosition * this.m_Sensitivity;
 
+            this.m_CameraViewAimTransform.Rotate(this.m_CameraTransform.right, _ScaledPosition.y, Space.World);
+            this.m_CameraViewAimTransform.Rotate(this.m_CameraTransform.up, _ScaledPosition.x, Space.World);
 
-            // Rotate the reference tr
+            Vector3 _UpVector = (Mathf.Abs(this.m_CameraViewAimTransform.forward.y) > 0.9f) 
+                ? this.m_CameraRigTransform.up 
+                : Vector3.up;
+
+            this.m_CameraRigTransform.rotation = Damp(this.m_CameraRigTransform.rotation,
+                                                    Quaternion.LookRotation(screenPosition.forward, _UpVector),
+                                                    this.m_CameraSmoothSpeed,
+                                                    Time.deltaTime);
         }
 
         public void SetThrustControl(float throttlePower)
         {
             this.m_ThrottlePower = throttlePower;
             this.m_Velocity = this.m_Speed * throttlePower * this.gameObject.transform.forward;
+        }
+
+        public void MoveFlyer(){
+
+        }
+
+        private Quaternion Damp(Quaternion a, Quaternion b, float lambda, float dt)
+        {
+            return Quaternion.Slerp(a, b, 1 - Mathf.Exp(-lambda * dt));
         }
 
         #endregion Methods
